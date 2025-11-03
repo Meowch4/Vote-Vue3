c8f40cb3d5c17e2b630fc2a2976ae1824a279b71
Matcha <Matcha@SuperPlatypus.com>
2025-11-03 10:23:02 +0800

    实现了匿名投票的单选/多选交互

--- src/views/Vote.vue
+++ src/views/Vote.vue
@@ -23,19 +23,21 @@
 
     <ul class="space-y-3">
       <li
-        class="relative px-4 flex items-center bg-white shadow h-16"
+        class="px-4 hover:bg-blue-300 cursor-pointer  bg-white shadow h-16"
         v-for="(option, idx) in voteInfo.options"
         :key="idx"
-        @click="handleOptionClick"
+        @click="handleOptionClick(option.optionId)"
       >
-        {{ option.content }}
-        <span class="pl-2">{{ isVotedByCurrentUser[option.optionId] ? '✔️' : '' }}</span>
-        <span class="ml-auto mr-2">{{ optionVotes[option.optionId].length }} 票</span>
-        <span>{{ optionPercentage[option.optionId] }}</span>
-        <div 
-        class="absolute bottom-0 left-4 right-4 bg-sky-500 h-[2px]"
-        :style="{width: optionPercentage[option.optionId]}"
-        ></div>
+      <div class="relative flex items-center h-16">
+          {{ option.content }}
+          <span class="pl-2">{{ optionChecked[option.optionId] ? '✔️' : '' }}</span>
+          <span class="ml-auto mr-2">{{ optionVotes[option.optionId].length }} 票</span>
+          <span class="w-14 text-right">{{ optionPercentage[option.optionId] }}</span>
+          <div 
+          class="absolute bottom-0 bg-sky-500 h-[2px]"
+          :style="{width: optionPercentage[option.optionId]}"
+          ></div>
+        </div>
       </li>
     </ul>
     <div class="flex justify-between items-center mx-4 h-12 text-base">
@@ -43,8 +45,10 @@
       <span> 吐个槽 | 举报投票 </span>
     </div>
     <button 
+    @click="submit"
     v-if="showCompleteButton"
-    class="bg-blue-500 text-white rounded m-4 p-2 flex justify-center text-lg"
+    :disabled="selectedOptionId.length == 0"
+    class="disabled:bg-gray-500 bg-blue-500 text-white rounded m-4 p-2 flex justify-center text-lg"
     >
     完成</button>
   </div>
@@ -105,6 +109,8 @@ var isVotedByCurrentUser = computed(() => {
   return result
 })
 
+// 实名，直接点击即可投票
+// 匿名，需要选择选项后再点击完成按钮
 var showCompleteButton = computed(() => {
   if (!voteInfo.vote.anonymous) {
     return false
@@ -117,20 +123,63 @@ var showCompleteButton = computed(() => {
   if (voteInfo.vote.anonymous && Object.values(isVotedByCurrentUser.value).some((it:any) => it == true)) {
     return false
   }
+  return true
 })
 
+var selectedOptionId = ref<number[]>([])
+
+// 返回对象，因为对勾的表达式是
+// isVotedByCurrentUser[option.optionId] ? '✔️' : '' 
+var optionChecked = computed(() => {
+  // 在未投票的时候选中哪个选项哪个选项打勾
+  // 需要把selectedOptionId（数组）转换成对象
+  if (showCompleteButton.value) {
+    let result:any = {}
+    for (let optionId of selectedOptionId.value) {
+      result[optionId] = true
+    }
+    return result
+  }
+  // 在投过票之后才通过isVoted来决定在哪个选项显示对勾
+  return isVotedByCurrentUser.value
+})
 
 function handleOptionClick(optionId: number) {
   // 非匿名，点击即发起请求
-  if (!voteInfo.anonymous) {
+  if (!voteInfo.vote.anonymous) {
     axios.post(`/vote/${voteInfo.vote.voteId}`, {
       optionIds: [optionId]
+    }).then(res => {
+      // console.log(res.data.result)
+      voteInfo.userVotes = res.data.result.userVotes
     })
   } else {
-    // 匿名投票点击只是选中该选项，点提交才发送请求，且不能再发送了
-
-
+    if (showCompleteButton.value) {
+      // 匿名投票点击只是选中该选项，点提交才发送请求，且不能再发送了
+      if (selectedOptionId.value.includes(optionId)) {
+        var idx = selectedOptionId.value.indexOf(optionId)
+        selectedOptionId.value.splice(idx, 1)
+      } else {
+        if (!voteInfo.vote.multiple) {
+          // 单选时只能选一项，所以一定是数组中只有一个元素
+          selectedOptionId.value = [optionId]
+        } else {
+          selectedOptionId.value.push(optionId)
+        }
+      }
+    } else {
+      alert('已投过票！')
+    }
   }
+}
+
+function submit() {
+  axios.post(`/vote/${voteInfo.vote.voteId}`, {
+    optionIds: selectedOptionId.value
+  }).then(res => {
+    // console.log(res.data.result)
+    voteInfo.userVotes = res.data.result.userVotes
+  })
 }
 
 
